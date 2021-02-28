e63f1ec56bb911bda2b4cae8d3b85546615f5312
Vedat ERMİŞ <vedatermis@hotmail.com>
2021-02-28 13:39:08 +0300

    commit

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
 import { connect } from "react-redux";
-import {Badge, Table} from "reactstrap";
+import {Badge, Table, Button} from "reactstrap";
 import {bindActionCreators} from "redux";
 import * as productActions from "../../redux/actions/productActions";
+import * as cartActions from "../../redux/actions/cartActions";
+import alertify from "alertifyjs";
 
 
 class ProductList extends Component {
 
     componentDidMount() {
         this.props.actions.getProducts();
+    }
+
+    addToCart = (product) => {
+        this.props.actions.addToCart({
+            quantity: 1,
+            product
+        });
+        alertify.notify(product.productName + " sepete eklendi");
     }
 
     render() {
@@ -29,6 +39,7 @@ class ProductList extends Component {
                         <th>Unit Price</th>
                         <th>Quantity Per Unit</th>
                         <th>Units In Stock</th>
+                        <th/>
                     </tr>
                     </thead>
                     <tbody>
@@ -40,6 +51,9 @@ class ProductList extends Component {
                                <td>{product.unitPrice}</td>
                                <td>{product.quantityPerUnit}</td>
                                <td>{product.unitsInStock}</td>
+                               <td>
+                                   <Button color="success" onClick={() => this.addToCart(product)}>+</Button>
+                               </td>
                            </tr>
                         ))
                     }
@@ -60,7 +74,8 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
     return {
         actions: {
-            getProducts: bindActionCreators(productActions.getProducts, dispatch)
+            getProducts: bindActionCreators(productActions.getProducts, dispatch),
+            addToCart: bindActionCreators(cartActions.addToCart, dispatch)
         }
     }
 }
