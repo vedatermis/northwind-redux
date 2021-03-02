f417c8e1d8e71792443d4bf2b783da3bbcf0fec1
Vedat ERMIS <vedatermis@hotmail.com>
2021-03-02 17:32:00 +0300

    commit

---
+++ src/components/toolbox/TextInput.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const TextInput = ({name, label, onChange, placeholder, value, error}) => {
+    let wrapperClass = "form-group";
+    if (error && error.length > 0) {
+        wrapperClass += " has-error";
+    }
+
+    return  (
+      <div className={wrapperClass}>
+          <label htmlFor = {name}>{label}</label>
+          <div className="field">
+              <input type="text" name={name} className="form-control" placeholder={placeholder} onChange={onChange} value={value}/>
+              {
+                  error && <div className="alert alert-danger">{error}</div>
+              }
+          </div>
+      </div>
+    );
+}
+
+export default TextInput;
--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -7,6 +7,42 @@ export function getProductsSuccess(products) {
     }
 }
 
+export function createProductSuccess(product) {
+    return {
+        type: actionTypes.CREATE_PRODUCT_SUCCESS,
+        payload: product
+    }
+}
+
+export function updateProductSuccess(product) {
+    return {
+        type: actionTypes.UPDATE_PRODUCT_SUCCESS,
+        payload: product
+    }
+}
+
+export function saveProductApi(product) {
+    return fetch("http://localhost:3001/products/" +(product.id || ""), {
+        method: product.id ? "PUT" : "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(product)
+    })
+        .then(handleResponse)
+        .catch(handleError);
+}
+
+export function saveProduct(product){
+    return function (dispatch) {
+        return saveProductApi(product).then(savedProduct => {
+            product.id
+                ? dispatch(updateProductSuccess(savedProduct))
+                : dispatch(createProductSuccess(savedProduct))
+        }).catch(error => {
+            throw error;
+        })
+    }
+}
+
 export function getProducts(categoryId) {
     return function (dispatch) {
         let url = "http://localhost:3001/products";
@@ -19,4 +55,18 @@ export function getProducts(categoryId) {
             .then(response => response.json())
             .then(result => dispatch(getProductsSuccess(result)));
     }
+}
+
+export async function handleResponse(response) {
+    if (response.ok){
+        return response.json();
+    }
+
+    const error = await response.text();
+    throw new Error(error);
+}
+
+export function handleError(error) {
+    console.log("bir hata oluştu");
+    throw error;
 }
